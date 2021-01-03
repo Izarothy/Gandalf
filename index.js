be87498efc2e65215ffd55d071211fce4c7738c7
Izarothy <izarothmail@gmail.com>
2021-01-03 22:33:12 +0100

    Gimli embed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.once('ready', () => {
 
 const AragornEmbed = new Discord.MessageEmbed()
     .setTitle('Aragorn')
-    .setDescription('Aragorn II, syn Arathorna II i Gilraeny, także znany jako Elessar i Obieżyświat, był szesnastym wodzem Dunedainów z Północy; później koronowany Królem Elessarem.')
+    .setDescription('Aragorn II, syn Arathorna II i Gilraeny, także znany jako Elessar i Obieżyświat, był szesnastym, ostatnim, wodzem Dunedainów z Północy; później koronowany Królem Elessarem. Był dziedzicem tronu Gondoru w prostej linii. Wyruszył na wyprawę z Drużyną Pierścienia, która skończyła się dla niego sukcesem. Po wojnie zjednoczył królestwa Arnoru i Gondoru, a także włączył w protekcję Shire. ')
     .setImage('https://www.indiewire.com/wp-content/uploads/2017/11/aragon-the-lord-of-the-rings.jpg')
     .setFooter('Tawerna Fantastyki, Sekcja Śródziemia')
     .setThumbnail('https://cdn.discordapp.com/attachments/484655342400307211/795401844112883742/pierscien.png')
@@ -20,6 +20,12 @@ const LegolasEmbed = new Discord.MessageEmbed()
     .setFooter('Tawerna Fantastyki, Sekcja Śródziemia')
     .setThumbnail('https://cdn.discordapp.com/attachments/484655342400307211/795401844112883742/pierscien.png')
 
+const GimliEmbed = new Discord.MessageEmbed()
+    .setTitle('Gimli')
+    .setDescription('Gimli, syn Gloina - krasnolud z Rodu Durina. Podczas gdy jego ojciec wyruszał z Thorinem by odzyskać Erebor, jemu nie pozwolono, gdyż - jako 62-latek - był za młody. Wyruszył z Drużyną Pierścienia na wyprawę do Mordoru, gdzie zaprzyjaźnił się na całe życie z Legolasem. Warto też wspomnieć, że został przyjacielem elfów, co było niesamowicie rzadkie wśród krasnoludów. Umiłował Aglarond, czyli jaskinie obok Helmowego Jaru i w 120 roku Czwartej Ery odpłynął do Amanu.')
+    .setImage('https://static.wikia.nocookie.net/lotr/images/e/ec/Gimli_-_FOTR.png/revision/latest?cb=20121008105956')
+    .setFooter('Tawerna Fantastyki, Sekcja Śródziemia')
+    .setThumbnail('https://cdn.discordapp.com/attachments/484655342400307211/795401844112883742/pierscien.png')
 
 client.on('message', message => {
     if (message.content.startsWith(`${prefix}aragorn`)) {
@@ -27,6 +33,9 @@ client.on('message', message => {
     }
     if (message.content.startsWith(`${prefix}legolas`)) {
         message.channel.send(LegolasEmbed);
+    }
+    if (message.content.startsWith(`${prefix}Gimli`)) {
+        message.channel.send(GimliEmbed);
     }
 
 });
