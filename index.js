fa095b258a6da2c5095831787673cbbe9f8cc13c
Izarothy <izarothmail@gmail.com>
2021-01-03 21:08:57 +0100

    footer

--- index.js
+++ index.js
@@ -10,17 +10,20 @@ const AragornEmbed = new Discord.MessageEmbed()
     .setTitle('Aragorn')
     .setDescription('Aragorn II, syn Arathorna II i Gilraeny, także znany jako Elessar i Obieżyświat, był szesnastym wodzem Dunedainów z Północy; później koronowany Królem Elessarem.')
     .setImage('https://www.indiewire.com/wp-content/uploads/2017/11/aragon-the-lord-of-the-rings.jpg')
+    .setFooter('Tawerna Fantastyki, Sekcja Śródziemia')
 
 const LegolasEmbed = new Discord.MessageEmbed()
     .setTitle('Legolas')
     .setDescription('Legolas, syn Thranduila, był elfickim księciem z Mrocznej Puszczy. Wyruszył w podróż razem z Drużyną Pierścienia, żeby zniszczyć Jedyny Pierścień. Jako elf miał doskonały wzrok, tym samym znacznie przysługując się Drużynie. Zaprzyjaźnił się z Gimlim pomimo różnic, jakie dzieliły ich rasy. Nic nie wiemy o jego matce. Na spotkanie u Elronda przyszedł jako wysłannik ojca. Po zakończeniu Wojny o Pierścień został na koronacji Aragorna i po zwiedzeniu wraz z Gimlim jaskiń Helmowego Jaru i Fangornu odpłynął do Amanu. ')
     .setImage('https://i.pinimg.com/originals/07/31/37/0731379032530492cec0984f06bf1318.jpg')
+    .setFooter('Tawerna Fantastyki, Sekcja Śródziemia')
+
 
 client.on('message', message => {
-    if (message.content === `${prefix}aragorn`) {
+    if (message.content.startsWith(`${prefix}aragorn`)) {
         message.channel.send(AragornEmbed);
     }
-    if (message.content === `${prefix}legolas`) {
+    if (message.content.startsWith(`${prefix}legolas`)) {
         message.channel.send(LegolasEmbed);
     }
 
