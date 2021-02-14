d714bbf41412866e8d99d830f7abdc7a25681439
Izarothy <izarothmail@gmail.com>
2021-02-14 18:07:00 +0100

    lotr -> mods

--- commands/minecraft.js
+++ commands/minecraft.js
@@ -7,18 +7,18 @@ module.exports = {
 
         let launcher = "https://skmedix.pl/sklauncher/downloads";
         let forge = "https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.15.2-31.2.47/forge-1.15.2-31.2.47-mdk.zip";
-        let mod = "https://www.curseforge.com/minecraft/mc-mods/the-lord-of-the-rings-mod-renewed";
+        let mod = "https://drive.google.com/drive/folders/1_6ECVByjO3bJ9EGfcJqjECC9K9lHyznm?usp=sharing";
 
         const Minecraft = new Discord.MessageEmbed()
             .setTitle('Poradnik i ważne linki do naszego serwera Minecraft')
             .setDescription('\n Polecany launcher non-premium: ' + launcher +
                 '\n \n Forge: ' + forge +
-                '\n \n Mod: ' + mod +
+                '\n \n Mody: ' + mod +
                 '\n \n IP: tawernasrodziemie.tasrv.com' +
                 '\n \n 1. Pobieracie launcher (oficjalny lub ten z wyżej)' +
                 '\n 2. Instalujecie wersję **1.15.2** Minecrafta' +
                 '\n 3. Po zainstalowaniu wychodzicie i instalujecie Forge z linku powyżej' +
-                '\n 4. Wchodzicie w %appdata%, .minecraft i wrzucacie plik .jar (mod) do folderu mods - jeśli go nie ma, po prostu trzeba stworzyć.' +
+                '\n 4. Wchodzicie w %appdata%, .minecraft i wrzucacie tam folder mods.' +
                 '\n Jeśli są jakieś problemy techniczne, choć nie powinno, prosimy pisać do @Izaroth#6666!'
             )
             .setFooter('Tawerna Fantastyki, Serwer Minecraft')
