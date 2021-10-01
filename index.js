99d7cd91f4efce18200942adf701513aa17d32a3
Izarothy <izarothmail@gmail.com>
2021-10-01 12:29:17 +0200

    Delete useless files, merge minecraft command to main file, code cleanup

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
 require("dotenv").config();
-const {Discord, Client, Intents} = require("discord.js");
+const {MessageEmbed, Client, Intents} = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES ]})
 const config = require("./config.json");
-const { prefix } = require("./config.json");
 const channels = require("./json/channels.json");
+const minecraftMessage = require('./json/messages.json').minecraft
 
 client.once("ready", () => {
   console.log("The bot is working.");
@@ -15,41 +15,55 @@ client.on("voiceStateUpdate", (previousVoiceState, currentVoiceState) => {
   let currentVoiceChannel = currentVoiceState.channel;
   let previousVoiceChannel = previousVoiceState.channel;
   let voiceChannelUser = currentVoiceState.member;
-
+  
   if (currentVoiceChannel === null || currentVoiceChannel === undefined) {
     let previousChannelIndex = channels.voice.indexOf(previousVoiceChannel.id)
     let previousTextChannel = client.channels.cache.get(channels.text[previousChannelIndex])
     previousTextChannel.permissionOverwrites.delete(voiceChannelUser)
     return;
   }
+
   if (currentVoiceChannel !== previousVoiceChannel) {
     let voiceChannelIndex = channels.voice.indexOf(currentVoiceChannel.id)
-        let currentTextChannel = client.channels.cache.get(channels.text[voiceChannelIndex])
-        currentTextChannel.permissionOverwrites.create(voiceChannelUser, {VIEW_CHANNEL: true});
-        if (previousVoiceChannel) {
-          let previousChannelIndex = channels.voice.indexOf(previousVoiceChannel.id);
-          let previousTextChannel = client.channels.cache.get(channels.text[previousChannelIndex]);
-          previousTextChannel.permissionOverwrites.delete(voiceChannelUser);
-        }
-  }
+    let currentTextChannel = client.channels.cache.get(channels.text[voiceChannelIndex])
+    currentTextChannel.permissionOverwrites.create(voiceChannelUser, {VIEW_CHANNEL: true});
 
+    if (previousVoiceChannel) {
+      let previousChannelIndex = channels.voice.indexOf(previousVoiceChannel.id);
+      let previousTextChannel = client.channels.cache.get(channels.text[previousChannelIndex]);
+      previousTextChannel.permissionOverwrites.delete(voiceChannelUser);
+    }
+  }
+  
 });
 
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
-
+  
   const reactToAMeme = () => {
     message.react("👍");
     message.react("👎");
+  }
+
+  if (message.content === '!minecraft' && message.channel.id === config.minecraft_id) {
+    const minecraftEmbed = new MessageEmbed()
+      .setTitle(minecraftMessage.title)
+      .setDescription(minecraftMessage.description)
+      .setColor(minecraftMessage.color)
+      .setThumbnail(minecraftMessage.thumbnail)
+
+      message.channel.send({embeds: [minecraftEmbed]})
+      message.delete();
   }
 
   if (message.channel.id === config.meme_id) {
     if (message.content.includes('http') || message.attachments.size > 0) {
       reactToAMeme();
     }
-      else {
-      message.delete(); }}
-
-});
-
-client.login(process.env.CLIENT_TOKEN);
+    else {
+      message.delete(); 
+    }}
+  });
+    
+    client.login(process.env.CLIENT_TOKEN);
+    
